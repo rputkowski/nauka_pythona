06db1ead65fa430a59ab2ee6b0c7ade7bfaaec26
rputkowski <rputkowski@users.noreply.github.com>
2022-05-29 13:20:34 +0200

    Nowe rzeczy

--- Klasy/pierwszaklasa.py
+++ Klasy/pierwszaklasa.py
@@ -9,6 +9,12 @@ class Paletka:
     def __init__(self, kolor):
         self.kolor_obiektu = kolor
         print(f"Utworzyliśmy obiekt o kolorze: {self.kolor_obiektu} - ID: {id(self)}")
+
+    def info(self):
+        print(f"Kolor obiektu to: {self.kolor_obiektu}")
+
+    def info_ex(self, nazwa):
+        print(f"Kolor obiektu {nazwa} to: {self.kolor_obiektu}")
 # tworzymy obiekt na podstawie klasy, podajemy nazwę obiektu (paletka_a) i wywołujemy konstruktor klasy (Paletka())
 # f-string
 # val = 'Python course'
@@ -28,3 +34,7 @@ def testklasy():
     print("*****************************************************************")
     print(f"Kolor dla paletka_a: {paletka_a.kolor_obiektu}")
     print(f"Kolor dla paletka_b: {paletka_b.kolor_obiektu}")
+    paletka_a.info()
+    paletka_b.info()
+    paletka_a.info_ex("a")
+    paletka_b.info_ex("b")
